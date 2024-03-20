14d726867dad4faf232a129dfe7f5ce4992200b7
kloyer <k.loyer07@gmail.com>
2024-03-20 12:54:45 +0100

    Default state

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -19,9 +19,9 @@ export default () => {
                 <form class="form-signin" data-testid="form-employee">
                   <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
                   <label for="inputEmail">Votre email</label>
-                  <input id="inputEmail" type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
                   <label for="inputPassword">Mot de passe</label>
-                  <input id="inputPassword" type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
+                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
                   <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
               </div>
@@ -32,10 +32,10 @@ export default () => {
               <div class="card-body">
                 <form class="form-signin" data-testid="form-admin">
                   <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
-                  <label for="inputEmailAdmin">Votre email</label>
-                  <input id="inputEmailAdmin" type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPasswordAdmin">Mot de passe</label>
-                  <input id="inputPasswordAdmin" type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
+                  <label for="inputEmail">Votre email</label>
+                  <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <label for="inputPassword">Mot de passe</label>
+                  <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
                   <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
                 </form>
               </div>
